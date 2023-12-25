3b27791357a4d364e036f9bacd23f58238cde625
Nirasha Jayalath <64356062+jayalath-jknr@users.noreply.github.com>
2023-12-25 18:27:18 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,45 +1,61 @@
-import validators, streamlit as st
+import validators
+import streamlit as st
+from PIL import Image
 from langchain.chat_models import ChatOpenAI
-from langchain.document_loaders import UnstructuredURLLoader
+from langchain.document_loaders import UnstructuredURLLoader, UnstructuredImageLoader
 from langchain.chains.summarize import load_summarize_chain
 from langchain.prompts import PromptTemplate
+import pytesseract
 
 # Streamlit app
 st.subheader('Vision Mate')
 
-# Get OpenAI API key and URL to be summarized
+# Get OpenAI API key and input type
 with st.sidebar:
-    openai_api_key = st.text_input("Gemini AI API key", value="", type="password")
-    st.caption("*If you don't have an Gemini AI API key, get it [here]().*")
-    model = st.selectbox("Gemini AI chat model", ("gpt-3.5-turbo", "gpt-3.5-turbo-16k"))
-    # st.caption("*If the article is long, choose gpt-3.5-turbo-16k.*")
-url = st.text_input("URL", label_visibility="collapsed")
+    openai_api_key = st.text_input("OpenAI API key", value="", type="password")
+    st.caption("*If you don't have an OpenAI API key, get it [here](https://platform.openai.com/account/api-keys).*")
+    model = st.selectbox("OpenAI chat model", ("gpt-3.5-turbo", "gpt-3.5-turbo-16k"))
+    st.caption("*If the article is long, choose gpt-3.5-turbo-16k.*")
+    input_type = st.selectbox("Input type", ("URL", "Image"))
 
 # If 'Summarize' button is clicked
-if st.button("Describe"):
+if st.button("Summarize"):
     # Validate inputs
-    if not openai_api_key.strip() or not url.strip():
-        st.error("Please provide the missing fields.")
-    elif not validators.url(url):
-        st.error("Please enter a valid URL.")
-    else:
-        try:
-            with st.spinner("Please wait..."):
+    if not openai_api_key.strip():
+        st.error("Please provide the OpenAI API key.")
+    elif input_type == "URL":
+        url = st.text_input("URL", label_visibility="collapsed")
+        if not url.strip() or not validators.url(url):
+            st.error("Please enter a valid URL.")
+    elif input_type == "Image":
+        image = st.file_uploader("Upload Image", type=["jpg", "jpeg", "png"])
+        if image is None:
+            st.error("Please upload an image.")
+
+    try:
+        with st.spinner("Please wait..."):
+            if input_type == "URL":
                 # Load URL data
                 loader = UnstructuredURLLoader(urls=[url])
-                data = loader.load()
-                
-                # Initialize the ChatOpenAI module, load and run the summarize chain
-                llm = ChatOpenAI(temperature=0, model=model, openai_api_key=openai_api_key)
-                prompt_template = """Write a summary of the following in 250-300 words:
+            elif input_type == "Image":
+                # Use Tesseract to extract text from the image
+                image = Image.open(image)
+                text_from_image = pytesseract.image_to_string(image)
+                loader = UnstructuredImageLoader(texts=[text_from_image])
+
+            data = loader.load()
+
+            # Initialize the ChatOpenAI module, load and run the summarize chain
+            llm = ChatOpenAI(temperature=0, model=model, openai_api_key=openai_api_key)
+            prompt_template = """Write a summary of the following in 250-300 words:
                     
-                    {text}
+                {text}
 
-                """
-                prompt = PromptTemplate(template=prompt_template, input_variables=["text"])
-                chain = load_summarize_chain(llm, chain_type="stuff", prompt=prompt)
-                summary = chain.run(data)
+            """
+            prompt = PromptTemplate(template=prompt_template, input_variables=["text"])
+            chain = load_summarize_chain(llm, chain_type="stuff", prompt=prompt)
+            summary = chain.run(data)
 
-                st.success(summary)
-        except Exception as e:
-            st.exception(f"Exception: {e}")
+            st.success(summary)
+    except Exception as e:
+        st.exception(f"Exception: {e}")
